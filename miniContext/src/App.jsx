f077abfa10afc7d41e6987d441ed2e869695dfbc
DINESH <starkdinesh2@gmail.com>
2024-06-22 09:42:30 +0000

    contextapi

--- miniContext/src/App.jsx
+++ miniContext/src/App.jsx
@@ -1,10 +1,15 @@
-import "./App.css"
+import "./App.css";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
+import UserContextProvider from "./context/UserContextProvider";
 
 const App = () => {
   return (
-    <>
-      <h1>React with DINESH</h1>
-    </>
+    <UserContextProvider>
+      <h1>DINESH</h1>
+      <Login />
+      <Profile />
+    </UserContextProvider>
   );
 };
 
--- miniContext/src/context/UserContextProvider.jsx
+++ miniContext/src/context/UserContextProvider.jsx
@@ -2,11 +2,12 @@ import react from "react";
 import UserContext from "./UserContext";
 
 const UserContextProvider = ({ children }) => {
-    const [user, setUser] = react.useState({})
-  return <UserContext.Provider value={{user, setUser}}>
-    {children}
-    </UserContext.Provider>;
+  const [user, setUser] = react.useState({});
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
 };
-
 
 export default UserContextProvider;
